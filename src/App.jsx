ce8de75f82fea61bc5c38d492f4bfc262404b230
RxGoodness <adedokuntoyyib@gmail.com>
2022-07-17 22:17:06 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Auth from "./pages/auth";
+import Dashboard from "./pages/dashboard";
+import Profile from "./pages/profile";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    let user = localStorage.getItem("user");
+    user && JSON.parse(user) ? setAuth(true) : setAuth(false);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("user", auth);
+  }, [auth]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Routes>
+      {!auth && (
+        <Route
+          path="/auth"
+          element={<Auth authenticate={() => setAuth(true)} />}
+        />
+      )}
 
-export default App
+      {auth && (
+        <>
+          <Route
+            path="/profile"
+            element={<Profile logout={() => setAuth(false)} />}
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </>
+      )}
+      <Route path="*" element={<Navigate to={auth ? "/profile" : "/auth"} />} />
+    </Routes>
+  );
+};
+
+export default App;
